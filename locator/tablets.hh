743bbd24d22eaec869f4a4d4a161ca6493a145c9
Sergey Zolotukhin <sergey.zolotukhin@scylladb.com>
2024-10-25 15:59:13 +0200

    Refactor `tablet_effective_replication_map::get_replica*` functions.
    1. Split the functionality of get_endpoints_for_reading into two functions:
    
    - `get_endpoints_for_reading(const tablet_id tablet)` returns the current
       read replica host ID set from Tablets. This change is necessary as part
       of introducing new replication factor calculation functions.
    - `get_endpoints_for_reading(const token& search_token)` uses the new function
       that inputs a Tablet id.
    
    2. Move `get_endpoints_for_reading`, `get_pending_endpoints`, and `get_replicas_for_write`
       logic for finding replicas in the Tablet map to the `tablet_map` class.
       This change is needed to build the replication factor map outside of the ERM
       and improves encapsulation of the `tablet_map` logic.

--- locator/tablets.hh
+++ locator/tablets.hh
@@ -428,6 +428,13 @@ public:
     bool needs_split() const;
 
     const locator::resize_decision& resize_decision() const;
+
+    const tablet_replica_set& get_replicas_for_reading(tablet_id tablet) const;
+
+    const tablet_replica_set& get_replicas_for_writing(tablet_id tablet) const;
+
+    const std::optional<tablet_replica>* get_pending_replica(tablet_id tablet) const;
+
 public:
     void set_tablet(tablet_id, tablet_info);
     void set_tablet_transition_info(tablet_id, tablet_transition_info);
